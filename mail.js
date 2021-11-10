45534e11d573486d06690de154793293196f28b4
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-11-10 17:46:17 +0100

    Fine mail

--- mail.js
+++ mail.js
@@ -1,1 +1,20 @@
-alert("ciao");
+const listaMail = ["qwerty@gmail.com", "wasd@gmail.com", "rosso@gmail.com", "bianco@gmail.com"];
+
+for(let i = 0; i < 4; i++){
+    console.log(listaMail[i]);
+}
+
+const aMail = prompt("inserisci la tua mail");
+console.log("La mail inserita è: " + aMail);
+
+let sentinel = false;
+for(let i = 0; i < 4 && sentinel == false; i++){
+    if(aMail == listaMail[i]){
+        console.log("SEI DENTRO");
+        sentinel = true;
+    }
+    else {
+        console.log("NON SEI DENTRO");
+        sentinel = true;
+    }
+}
